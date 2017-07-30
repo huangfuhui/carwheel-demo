fe518332332a188822499d18942536221569aeb4
huangfuhui <huangfuhui@outlook.com>
2017-07-30 11:13:06 +0800

    数据对象

---
+++ src/main/java/cn/carwheel/bean/Data.java
@@ -0,0 +1,20 @@
+package cn.carwheel.bean;
+
+/**
+ * 数据对象
+ */
+public class Data {
+
+    /**
+     * 模型数据
+     */
+    private Object model;
+
+    private Data(Object model) {
+        this.model = model;
+    }
+
+    public Object getModel() {
+        return model;
+    }
+}
